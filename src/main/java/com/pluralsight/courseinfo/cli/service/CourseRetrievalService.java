2b1e94bf44ef30da342ed8558d4ade8581b92e92
Miles Lin <milesasp@gmail.com>
2023-12-23 22:46:56 +0800

    learn switch syntax

--- src/main/java/com/pluralsight/courseinfo/cli/service/CourseRetrievalService.java
+++ src/main/java/com/pluralsight/courseinfo/cli/service/CourseRetrievalService.java
@@ -8,7 +8,10 @@ import java.net.http.HttpResponse;
 
 public class CourseRetrievalService {
     private static final String PS_URI = "https://app.pluralsight.com/profile/data/author/%s/all-content";
-    private static final HttpClient CLIENT = HttpClient.newHttpClient();
+    private static final HttpClient CLIENT = HttpClient
+            .newBuilder()
+            .followRedirects(HttpClient.Redirect.ALWAYS)
+            .build();
     public String getCourse(String authorId){
         var request = HttpRequest
                 .newBuilder(URI.create(PS_URI.formatted(authorId)))
@@ -16,7 +19,11 @@ public class CourseRetrievalService {
                 .build();
         try {
             HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-            return response.body();
+            return switch (response.statusCode()){
+                case 200 -> response.body();
+                case 404 -> "";
+                default -> throw new RuntimeException("Pluralsight API call failed with statuc code "+ response.statusCode());
+            };
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException("Could not call Pluralsight API", e);
         }
